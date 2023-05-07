bd546234e8749abb737c1787f2f51b70577c948a
Diego Guilherme <diegoguilherme752@oultook.com>
2023-05-07 20:14:47 -0300

    feat: base skill page

---
+++ src/database/skills.db.ts
@@ -0,0 +1,76 @@
+import htmlIcon from "../../public/assets/html-vector.svg";
+import cssIcon from "../../public/assets/css-vector.svg";
+import jsIcon from "../../public/assets/js-vector.svg";
+import tsIcon from "../../public/assets/ts-vector.svg";
+import reactIcon from "../../public/assets/react-vector.svg";
+import nextIcon from "../../public/assets/nextjs-png.svg";
+import scIcon from "../../public/assets/sc-png.svg";
+import twIcon from "../../public/assets/tailwind-png.svg";
+import djIcon from "../../public/assets/dj-png.svg";
+import pyIcon from "../../public/assets/py-vector.svg";
+import exIcon from "../../public/assets/ex-png.svg";
+import postIcon from "../../public/assets/post-png.svg";
+import gitIcon from "../../public/assets/git-png.svg";
+import nodeIcon from "../../public/assets/node-png.svg";
+
+
+const skillsDb = [
+    {
+      img: htmlIcon,
+      name: "HTML5",  
+    },
+    {
+      img: cssIcon,
+      name: "CSS3",  
+    },
+    {
+      img: jsIcon,
+      name: "Javascript",  
+    },
+    {
+      img: tsIcon,
+      name: "Typescript",  
+    },
+    {
+      img: reactIcon,
+      name: "ReactJs",  
+    },
+    {
+      img: nextIcon,
+      name: "NextJs",  
+    },
+    {
+      img: twIcon,
+      name: "Tailwind",  
+    },
+    {
+      img: scIcon,
+      name: "Styled-c",  
+    },
+    {
+      img: nodeIcon,
+      name: "NodeJs",  
+    },
+    {
+      img: exIcon,
+      name: "Express.js",  
+    },
+    {
+      img: postIcon,
+      name: "Postgresql",  
+    },
+    {
+      img: pyIcon,
+      name: "Python",  
+    },
+    {
+      img: djIcon,
+      name: "Django",  
+    },
+    {
+      img: gitIcon,
+      name: "Git",  
+    }
+]
+
+export default skillsDb
