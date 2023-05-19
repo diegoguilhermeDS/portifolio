0bbede20cfbffbebe42a9ab8db94d8dd4c67878e
Diego Guilherme <diegoguilherme752@oultook.com>
2023-05-19 10:41:54 -0300

    feat: initial styled

--- src/pages/skills/index.tsx
+++ src/pages/skills/index.tsx
@@ -6,6 +6,7 @@ import skillsDb from "@/database/skills.db";
 export default function skills() {
   return (
     <main className="flex justify-around items-center container mx-auto min-h-screen pt-24">
+      <h2 className="font-normal text-2xl tracking-wide text-brand-200">Essas são algumas das tecnologias e Ferramentas que possuo conhecimento no momento.</h2>
       <ul className="grid grid-cols-7 gap-x-14 gap-y-14">
         {skillsDb.map((skill, index) => 
           <li key={index} className="group flex flex-col items-center gap-4 grayscale transition ease-in-out duration-700 delay-150  hover:grayscale-0 ">
