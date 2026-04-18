c15737c4ddcd793e34160881a20b8c799c9aa096
Diego Silva <diegoguilherme752@outlook.com>
2026-04-17 21:05:54 -0300

    chore: init new portifolio (Next.js + clean architecture)

--- src/database/navbar.ts
+++
@@ -1,9 +0,0 @@
-const navBar = [
-  { name: "Início", href: "/" },
-  { name: "Sobre", href: "/about" },
-  { name: "Projetos", href: "/project" },
-  { name: "Habilidades", href: "/skills" },
-  { name: "Contato", href: "/contact" },
-];
-
-export default navBar;
